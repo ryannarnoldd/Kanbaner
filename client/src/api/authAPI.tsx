d711a34b1638adac375eb1e4549deb1069aa844f
ryannarnoldd <ryannarnoldd@gmail.com>
2024-11-21 12:43:44 -0500

    Added auth to api routes and created login

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,29 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch ('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    });
+    
+    if (!response.ok) {
+      const data = await response.json();
+      return data;
+
+    } else {
+
+      const errorData = await response.json();
+      throw new Error(errorData);
+    }
+
+  }
+  catch (err) {
+    return Promise.reject('Could not fetch user info');
+  }
 }
-
-
 
 export { login };

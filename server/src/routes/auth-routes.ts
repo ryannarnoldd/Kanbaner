c88945ba73851b1a8c9e23a1a148b3484100ae62
ryannarnoldd <ryannarnoldd@gmail.com>
2024-11-21 12:39:59 -0500

    Fixed authentication routes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,28 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: {username},
+  });
+
+  if (!user) {
+    return res.status(401).json({message: 'Authentication Failed'});
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(403).json({message: 'No Authentication'});
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || 'ex';
+
+  const token = jwt.sign({username}, secretKey, {expiresIn: '1h'});
+
+  return res.json({token});
+
 };
 
 const router = Router();
